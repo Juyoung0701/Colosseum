193fd42588d7b522e2e14d7f5e9675f9f1ddbf2a
Juyoung Lee <ljy070100@gmail.com>
2020-07-10 21:14:33 +0900

    회원가입 성공/실패 여부 처리. 실패시 실패 사유를 서버가 알려주는대로 출력 (Toast로)

--- app/src/main/java/kr/co/namu/colosseum/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/namu/colosseum/utils/ServerUtil.kt
@@ -10,67 +10,112 @@ class ServerUtil {
 
     //    액티비티에서 서버의 응답을 받았을때 처리할 코드를 담아두는 도구.
     interface JsonResponseHandler {
-
-        fun onResponse(json: JSONObject)
+        fun onResponse(json : JSONObject)
     }
 
     companion object {
 
-//        서버 접근 주소 담는 변수
-
+        //        서버 접근 주소 담는 변수
         private val BASE_URL = "http://15.165.177.142"
 
 //        로그인 요청을 해주는 함수 => 화면에서 입력한 아이디/비번 받아야함.
 
-        fun postRequestLogin(
-            context: Context,
-            id: String,
-            pw: String,
-            handler: JsonResponseHandler?
-        ) {
+        fun postRequestLogin(context: Context, id:String, pw:String, handler:JsonResponseHandler?) {
 
-//    서버 통신 담당 변수
-
+//            서버 통신 담당 변수
             val client = OkHttpClient()
-//    어느 주소로 통신 할지
+
+//            어느 주소로 통신할지
             val urlStr = "${BASE_URL}/user"
 
-//    서버에 들어갈 짐을 FormData에 담자 (POST ? PUT ? PATCH에서 이 방식)
+//            서버에 들고갈 짐을 FormData에 담자 (POST / PUT / PATCH에서 이 방식)
             val formData = FormBody.Builder()
                 .add("email", id)
                 .add("password", pw)
                 .build()
 
-//    요청 정보를 종합하는 변수
+//            요청 정보를 종합하는 변수
             val request = Request.Builder()
                 .url(urlStr)
                 .post(formData)
                 .build()
 
-//    실제 API호출
+//            실제 API호출
             client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
 
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-//            실제 서버 응답이 돌아왔을때 실행됨.
-//            응답 내용을 저장
+//                    실제 서버 응답이 돌아왔을때 실행됨.
+//                    응답 내용을 저장
                     val bodyStr = response.body?.string()
 
-//            이 내용으로 Json객체 생성
+//                    이 내용으로 Json객체 생성
+
                     val json = JSONObject(bodyStr)
 
                     Log.d("서버 응답 내용", json.toString())
 
-//            handler 변수에 응답처리 코드가 들어있다면 실행
+//                    hanlder 변수에 응답처리 코드가 들어있다면 실행
                     handler?.onResponse(json)
-                }
 
+                }
 
             })
 
+        }
+
+//        회원가입 요청을 해주는 함수 => 아이디 / 비번 / 닉네임을 받자.
+
+        fun putRequestSignUp(context: Context, id:String, pw:String, nick:String, handler:JsonResponseHandler?) {
+
+//            서버 통신 담당 변수
+            val client = OkHttpClient()
+
+//            어느 주소로 통신할지
+            val urlStr = "${BASE_URL}/user"
+
+//            서버에 들고갈 짐을 FormData에 담자 (POST / PUT / PATCH에서 이 방식)
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .add("nick_name", nick)
+                .build()
+
+//            요청 정보를 종합하는 변수
+            val request = Request.Builder()
+                .url(urlStr)
+                .put(formData)
+                .build()
+
+//            실제 API호출
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                    실제 서버 응답이 돌아왔을때 실행됨.
+//                    응답 내용을 저장
+                    val bodyStr = response.body?.string()
+
+//                    이 내용으로 Json객체 생성
+
+                    val json = JSONObject(bodyStr)
+
+                    Log.d("서버 응답 내용", json.toString())
+
+//                    hanlder 변수에 응답처리 코드가 들어있다면 실행
+                    handler?.onResponse(json)
+
+                }
+
+            })
 
         }
+
+
     }
+
 }

f35fc9ebbb55fa7b02b55b1d7f6c43cc750de9fb
Juyoung Lee <ljy070100@gmail.com>
2020-07-11 18:17:57 +0900

    서버에서 주제 목록 가져와서 ArrayList에 추가

--- app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
@@ -3,11 +3,15 @@ package kr.co.namu.colosseum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.namu.colosseum.data.Topic
 import kr.co.namu.colosseum.data.User
 import kr.co.namu.colosseum.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<Topic>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +24,36 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 //        getUserInfoFromServer()
+
+        getTopicListFromServer()
+    }
+
+//    진짜로 서버에서 토론목록 받아오기
+
+    fun getTopicListFromServer(){
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+                val topics = data.getJSONArray("topics")
+
+                for(i in 0..topics.length()-1) {
+                    val topicJson = topics.getJSONObject(i)
+
+                    val topic = Topic.getTopicFromJson(topicJson)
+
+//                    바꿔준 topic을 목록에 추가
+                    mTopicList.add(topic)
+
+                }
+
+//                목록을 모두 추가했으면 리스트뷰 새로고침
+
+            }
+
+        })
+
     }
 
 //    임시로 하는 작업. => 서버에서 내 정보를 받아와서 닉네임 뿌려주기
